f2292e81c7d8e2e00d10a223cc3537fe9efbeebe
NXA-Kved0 <ig.cavedo@duocuc.cl>
2026-05-12 01:08:26 -0400

    inventario

---
+++ Backend/ms-inventario/src/main/java/com/smartlogix/inventario/model/Producto.java
@@ -0,0 +1,29 @@
+package com.smartlogix.inventario.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private Integer stock;
+    private Double precio;
+
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public Integer getStock() { return stock; }
+    public void setStock(Integer stock) { this.stock = stock; }
+
+    public Double getPrecio() { return precio; }
+    public void setPrecio(Double precio) { this.precio = precio; }
+}
